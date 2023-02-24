8fef0a1eb11e44c126ec43f22853f4e5ba8f4738
GrayHut <ondiekiowen99@gmail.com>
2023-02-24 12:01:06 +0300

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *_isupper() - determines if a character is uppercase or lowercase
+ *retun : 0 or 1
+ */
+
+int _isupper(char c)
+
+{
+	for (c >= 'a'; c <= 'z'; c++)
+
+	{
+		return (0);
+	}
+
+	for (c >= 'A'; c <= 'Z'; c++)
+
+	{
+		return (1);
+	}
+}
