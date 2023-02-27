776cb509ad3aff9c46c0aac15fd95fc0f36e8d58
GrayHut <ondiekiowen99@gmail.com>
2023-02-27 21:33:05 +0300

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen - returns size of a string.
+ *@s: pointer to character integer;
+ *Return: size of string.
+ */
+
+
+int _strlen(char *s)
+{
+	return (sizeof(s));
+
+}
