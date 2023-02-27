b8814416a1034473c581c0637724685fa4bd9af3
GrayHut <ondiekiowen99@gmail.com>
2023-02-27 22:06:54 +0300

    2-strlen.c v2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- *_strlen - returns size of a string.
- *@s: pointer to character integer;
- *Return: size of string.
+ * _strlen - returns size of a string.
+ * @s: pointer to character integer;
+ * Return: size of string.
  */
-
 
 int _strlen(char *s)
 {
-	return (sizeof(s));
+	int l = 0;
 
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
 }
