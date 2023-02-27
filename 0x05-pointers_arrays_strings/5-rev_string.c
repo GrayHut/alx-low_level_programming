7d07cd75887695d9c8d20839622b828a018d1d9c
GrayHut <ondiekiowen99@gmail.com>
2023-02-28 01:32:02 +0300

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reveses string.
+ * @s: string to be printed.
+ * Return nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+
+	l, i = 0;
+
+	while (s[i++])
+		l++;
+
+	for (i = l - 1; i >= l / 2; i--)
+	{
+		temp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = temp;
+	}
+
+}
