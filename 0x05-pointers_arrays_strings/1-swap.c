ff3d42b8d61658793c7b7c75c4ec4e6ff3ebf0ec
GrayHut <ondiekiowen99@gmail.com>
2023-02-27 21:06:59 +0300

    1-swap.c v4

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- *wap_int(int *a, int *b) - swaps value of address of pointer 'a'
- *with 'b' and viceversa.
-
- *@a and @b: - are integer pointers
- *Return: always 0
+ * swap_int - swaps value of address of pointer 'a'
+ * with 'b' and viceversa.
+ *
+ * @a: pointer to an integer data-type;
+ * @b: pointer to an integer data-type;
+ * Return: always 0.
  */
 
 void swap_int(int *a, int *b)
 {
 	int prov;
+
 	prov = *a;
 	*a = *b;
-	prov = *b;
+	*b = prov;
 }
