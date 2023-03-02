06fb4393d8558a36a43b990f582c90947739a63f
GrayHut <ondiekiowen99@gmail.com>
2023-03-03 02:26:32 +0300

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - takes in two string arguments and concatinates them
+ * the conacatinated string ends with a null byte
+ * @dest: destination of final string
+ * @src: are the strings to be concatinated
+ *
+ * Return: pointer to the resulting string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
