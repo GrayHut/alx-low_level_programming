086af604e302e3b754dd02c3c01b2ad7b2f8c060
GrayHut <ondiekiowen99@gmail.com>
2023-03-07 00:58:49 +0300

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr-> function to be implemented
+ * @s: string to be analysed
+ * @c: character to be found
+ * Return: string character equal to c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s[i]);
+		}
+		else if (s[i] != c)
+		{
+			return (NULL);
+		}
+	}
+}
