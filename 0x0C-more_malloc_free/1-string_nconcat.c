49eaeaf00bd59f83a6bbd9fd110e1572044dc8a3
GrayHut <ondiekiowen99@gmail.com>
2023-03-17 04:01:31 +0300

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
  *string_nconcat-> function concatinates two strings @s1 and @s2.
@@ -9,43 +10,28 @@
  *
  *Return: pointer to resulting string s1
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *tmp;
-	int i, j = 0;
+	unsigned int i, j, s1Str_len = strlen(s1), s2Str_len = strlen(s2);
 
-	int s1Str_len = strlen(s1);
-	int s2Str_len = strlen(s2);
+	if (n < s2Str_len){
+		tmp = malloc(sizeof(char) * (s1Str_len + n + 1));}
+	else{
+		tmp = malloc(sizeof(char) * (s1Str_len + s2Str_len + n + 1));}
+	if (!tmp){
+		return (NULL);}
+	i = 0;
 
-	if (n < s2Str_len)
-	{
-		tmp = malloc(sizeof(char) * (s1Str_len + n + 1));
-	}
-	else
-	{
-		tmp = malloc(szeof(char) * (s1Str_len + s2Str_len + n + 1));
-	}
-
-	if (!tmp)
-	{
-		return (NULL);
-	}
-
-	while (i < s1Str_len)
-	{
+	while (i < s1Str_len){
 		tmp[i] = s1[i];
-	}
+		i++;}
+	j = 0;
 
-	while (n < s2Str_len && i < (s1Str_len + n))
-	{
-		tmp[i++] = s2[j++];
-	}
-
-	while (n >= s2Str_len && i < (s1Str_len + s2Str_len))
-	{
-		tmp[i++] = s2[j++];
-	}
+	while (n < s2Str_len && i < (s1Str_len + n)){
+		tmp[i++] = s2[j++];}
+	while (n >= s2Str_len && i < (s1Str_len + s2Str_len)){
+		tmp[i++] = s2[j++];}
 
 	tmp[i] = '\0';
 
