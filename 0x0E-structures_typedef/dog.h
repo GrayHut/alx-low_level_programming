c29953f8471a5332aa4c6e3522a3287d3d248de6
GrayHut <ondiekiowen99@gmail.com>
2023-03-21 23:57:01 +0300

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog-> this structure holds the attributes of a dog.
+ *@name: member1
+ *@age: member2
+ *@owner: member3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
