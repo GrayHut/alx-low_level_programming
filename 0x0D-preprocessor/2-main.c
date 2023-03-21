78ce9aec121fdd0c7f0c4f75fc7ac33d733824cc
GrayHut <ondiekiowen99@gmail.com>
2023-03-21 13:42:33 +0300

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *main-> prints name of the file it was compiled from
+ * followed by a new line
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	_putchar("%s\n", _FILE_);
+	return (0);
+}
