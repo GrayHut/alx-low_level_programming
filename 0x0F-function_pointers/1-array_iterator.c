e10dd18878923e577998966940ec526b28f0d0c8
GrayHut <ondiekiowen99@gmail.com>
2023-03-23 02:31:18 +0300

    1. If you spend too much time thinking about a thing, you'll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ *array_iterator-> function that takes array, size and function pointer
+ *parameters.
+ *@array:array of elements to be converted.
+ *@size:number of elements in the array.
+ *@action:pointer to functions.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int j;
+
+	if (size == NULL || action == NULL)
+	{
+		return;
+	}
+
+	for (j = 0; j < size; j++)
+	{
+		action(array[j]);
+	}
+}
