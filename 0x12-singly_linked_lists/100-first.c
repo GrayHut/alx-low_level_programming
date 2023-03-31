9013aecdb337d4c64fbaa868a11e5c214bced9a3
GrayHut <ondiekiowen99@gmail.com>
2023-04-01 02:55:42 +0300

    5.The hare and the tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 /**
- *print_b4_main-> function prints message before main() function.
  *@first:function executed before main.
  *Return:nothing
  */
 
-void print_b4_main((constructor) first)
+void __attribute__((constructor) first)
 {
 	printf("You're beat! and yet, you must allow, \nI bore my house upon my
 back!\n");
