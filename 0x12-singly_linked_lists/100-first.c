1e939fe95001e31abdc9d968d9d9847162888969
GrayHut <ondiekiowen99@gmail.com>
2023-04-01 02:58:21 +0300

    5.The hare and the tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  *Return:nothing
  */
 
-void __attribute__((constructor)) first()
+void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow, \nI bore my house upon my
 back!\n");
