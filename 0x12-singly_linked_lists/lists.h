ac3280666435d04bdf463a4a24c2840358a6d813
GrayHut <ondiekiowen99@gmail.com>
2023-03-31 21:08:41 +0300

    0. Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,8 +11,8 @@ typedef struct list_s
 {
 	char str;
 	int len;
-	struct list_s* next;
-}list_t;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
