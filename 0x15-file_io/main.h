4a9cbbde1f8e314357dd49c29013ca51ad5ff865
GrayHut <ondiekiowen99@gmail.com>
2023-04-11 04:37:45 +0300

    display info in the ELF header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
