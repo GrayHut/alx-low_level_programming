0cf0ec599803a2bc8e392b13767158e3cbe09236
GrayHut <ondiekiowen99@gmail.com>
2023-04-11 03:19:42 +0300

    copies one files contents to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,91 @@
+#include "main.h"
+
+/**
+ *create_mem-> creates some 1024 bytes for a buffer.
+ *@file: file buffer.
+ *
+ *Return: pointer to allocated buffer
+ */
+
+char *create_mem(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - function  closes file descriptors.
+ * @fd: file descriptor to be closed.
+ */
+void close_file(int f)
+{
+	int c;
+
+	c = close(f);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f);
+		exit(100);
+	}
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	int src, dest, j, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	src = open(argv[1], O_RDONLY);
+	j = read(from, buffer, 1024);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || j == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, j);
+
+
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		j = read(src, buffer, 1024);
+		dest = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (j > 0);
+
+	free(buffer);
+	close_file(src);
+	close_file(dest);
+
+	return (0);
+}
